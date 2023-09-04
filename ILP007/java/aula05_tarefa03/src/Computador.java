f25c5adc5cd127ccca3958adff87762f948c99a6
Leonardo Setti <srvsecsub@protonmail.com>
2023-09-03 21:30:55 -0300

    java POO -- activitiesSun Sep  3 09:30:55 PM -03 2023 fromLinux archfidel 6.4.11-arch2-1

---
+++ ILP007/java/aula05_tarefa03/src/Computador.java
@@ -0,0 +1,14 @@
+
+public class Computador extends Digital{
+	
+	private String placaMae;
+
+	public String getPlacaMae() {
+		return placaMae;
+	}
+
+	public void setPlacaMae(String placaMae) {
+		this.placaMae = placaMae;
+	}
+
+}
