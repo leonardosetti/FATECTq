b6b6c225a08bfb04e6865fde372291b01323d41a
Leonardo Setti <srvsecsub@protonmail.com>
2023-09-03 21:06:45 -0300

    _postsSun Sep  3 09:06:45 PM -03 2023 fromLinux archfidel 6.4.11-arch2-1

---
+++ ILP007/java/aula05_tarefa04/src/Almoxarifado.java
@@ -0,0 +1,13 @@
+
+public class Almoxarifado extends Administrativo {
+	
+	private String funcao;
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}	
+}
