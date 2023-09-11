8a055d5c40fd8c5b4d8b090368b65a60c92fd589
LEONARDO  DAVID SILVA SETTI <0220482313029@fatectq.local>
2023-09-11 20:18:54 -0300

    print aula06 atividade 02 POO (Packages) - from Fatec

--- ILP007/java/aula06_atividade02/src/academico/Disciplina.java
+++ ILP007/java/aula06_atividade02/src/academico/Disciplina.java
@@ -1,5 +1,36 @@
 package academico;
 
 public class Disciplina {
+	
+	private String sigla;
+	private String nome;
+	private int cargaHoraria;
+	// private String ementa;
+	
+	
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+//	public String getEmenta() {
+//		return ementa;
+//	}
+//	public void setEmenta(String ementa) {
+//		this.ementa = ementa;
+//	}
 
 }
