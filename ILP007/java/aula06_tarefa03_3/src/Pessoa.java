87cf5ea7c481c4e9e75d136255321b6525b70f6f
Leonardo Setti <srvsecsub@protonmail.com>
2023-09-11 16:41:16 -0300

    tarefa Java (POO) Mon Sep 11 04:41:15 PM -03 2023 fromLinux archfidel 6.4.12-arch1-1

---
+++ ILP007/java/aula06_tarefa03_3/src/Pessoa.java
@@ -0,0 +1,19 @@
+
+public class Pessoa {
+	
+	private String nome;
+	private Endereco endereco;
+	public String getNome() {
+		return nome;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+}
