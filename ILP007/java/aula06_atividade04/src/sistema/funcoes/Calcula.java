40a4481e80d49b8aa414415a9c94592cefcbf22f
LEONARDO  DAVID SILVA SETTI <lleonardo.setti@fatec.sp.gov.br>
2023-09-11 21:49:46 -0300

    :lab_tube: + print aula06 atividade04/05 POO - From Fatec

---
+++ ILP007/java/aula06_atividade04/src/sistema/funcoes/Calcula.java
@@ -0,0 +1,28 @@
+package sistema.funcoes;
+
+public class Calcula {
+
+	private String id;
+	private String descricao;
+	private String autor;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+}
---
+++ ILP007/java/aula06_atividade05/src/externo/Vendedor.java
@@ -0,0 +1,28 @@
+package externo;
+
+public class Vendedor {
+
+	private String codigo;
+	private String nome;
+	private double comissao;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getComissao() {
+		return comissao;
+	}
+	public void setComissao(double comissao) {
+		this.comissao = comissao;
+	}
+
+}
