73a5a422e106bf9266becba61a24c5c8933aafc6
LEONARDO  DAVID SILVA SETTI <0220482313029@fatectq.local>
2023-09-14 21:13:48 -0300

    add exercicio C

---
+++ ILP010/C/arrays/prog.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main (){
+
+int _tamanhoVetor, int _vetor_01[_tamanhoVetor];
+
+printf("Insira o tamanho do vetor \n");
+scanf("%d", &_tamanhoVetor);
+
+_vetor_01[_tamanhoVetor] = 3;
+
+
+printf("%d, e %d", _vetor_01;
+
+
+return (0);
+}
