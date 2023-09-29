b6a760b0c23fef54d1220a50a40d7548cb9ee9f1
Leonardo Setti <srvsecsub@protonmail.com>
2023-09-29 14:29:08 -0300

    POO - Atividade atrasada (falta) Fri Sep 29 02:29:08 PM -03 2023 fromLinux archfidel 6.5.3-arch1-1

---
+++ ILP007/java/aula09_atividade02/src/Motorista.java
@@ -0,0 +1,19 @@
+
+public class Motorista extends Funcionario{
+
+	private String cnh;
+	private double adicional;
+	
+	public String getCnh() {
+		return cnh;
+	}
+	public double getAdicional() {
+		return adicional;
+	}
+	public void setCnh(String cnh) {
+		this.cnh = cnh;
+	}
+	public void setAdicional(double adicional) {
+		this.adicional = adicional;
+	}
+}
