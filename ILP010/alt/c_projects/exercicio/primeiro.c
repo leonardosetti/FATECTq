2fc5c40fad2a055d07218e967e63699172cf087c
Leonardo Setti - Black USB <devnull-vortex@protonmail.com>
2023-10-06 14:17:11 -0300

    add avaliação C (desastre?)

---
+++ ILP010/alt/c_projects/exercicio/primeiro.c
@@ -0,0 +1,16 @@
+/*
+ * primeiro.c
+ *
+ *  Created on: Oct 6, 2023
+ *      Author: leonardo
+ */
+
+# include <stdio.h>
+
+int main() {
+
+	printf("\nFunciona no eclipse\n");
+	fflush(stdin);
+	getchar();
+
+}
