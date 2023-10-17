814dff15e8cee13c15aaa04bfa4263cf4f58d3c9
LEONARDO  DAVID SILVA SETTI <lleonardo.setti@fatec.sp.gov.br>
2023-10-16 21:11:36 -0300

    Aula 11 atividade 02 Mon Oct 16 21:11:36     2023 - 0220482313029 / MINGW64_NT-10.0-19042 3.3.4-341.x86_64 x86_64

---
+++ ILP007/java/aula11_atividade02/src/Principal.java
@@ -0,0 +1,64 @@
+
+public class Principal {
+
+	Veiculo v1, v2, v3;
+	Estacionamento e1;
+
+	public void inputData() {
+
+		v1 = new Veiculo();
+		v2 = new Veiculo();
+		v3 = new Veiculo();
+
+		v1.setPlaca("EXW34O2");
+		v1.setModelo("Hilux SW4");
+		v1.setCor("Branca");
+
+		v2.setPlaca("ZPC42U3");
+		v2.setModelo("Chevrolet Astra");
+		v2.setCor("Amarela");
+
+		v3.setPlaca("NTP36I2");
+		v3.setModelo("Ford Fusion");
+		v3.setCor("Prata");
+
+		Veiculo vetorEstacionamento[];
+		vetorEstacionamento = new Veiculo[3];
+		vetorEstacionamento[0] = v1;
+		vetorEstacionamento[1] = v2;
+		vetorEstacionamento[2] = v3;
+
+		e1 = new Estacionamento();
+		e1.setNome("Palácio dos Autos");
+		e1.setEndereco("Av. Nunes Malta, 325 - Centro / Rio Claro-SP");
+		e1.setVeiculo(vetorEstacionamento);
+
+	}
+
+	public void outputData() {
+
+		System.out
+				.println("----------------------Estacionamento " + e1.getNome() + "---------------------------------");
+		System.out.println("Endereço:		" + e1.getEndereco());
+		System.out.println();
+		System.out.println("Veículos estacionados:");
+		System.out.println();
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Modelo:		" + e1.getVeiculo()[i].getModelo());
+			System.out.println("Placa:		" + e1.getVeiculo()[i].getPlaca());
+			System.out.println("Cor:		" + e1.getVeiculo()[i].getCor());
+			System.out.println("---------------------------------------------------------------------------------");
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Principal p = new Principal();
+		p.inputData();
+		p.outputData();
+
+	}
+
+}
