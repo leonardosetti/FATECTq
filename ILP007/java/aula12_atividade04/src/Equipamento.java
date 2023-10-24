2e81e94bb30d4a0412a315ac3d779df3a233798e
Leonardo Setti <srvsecsub@protonmail.com>
2023-10-24 08:13:08 -0300

    :pencil: Aula12 Atividade04: atrasada Tue Oct 24 08:13:08 AM -03 2023 from [Linux archfidel 6.5.7-zen1-1-zen][/home/leonardo/lab/fatec_work][90e9cac828329eeb47ae21169e718e46a8193aa9df47ad0034364b5791b5ae13  -]

--- ILP007/java/aula12_atividade04/src/Equipamento.java
+++ ILP007/java/aula12_atividade04/src/Equipamento.java
@@ -1,4 +1,50 @@
 
 public class Equipamento {
 
+	private String id;
+	private String nome;
+	private String modelo;
+	private Laboratorio laboratorio;
+	private Pecas pecas[];
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public Laboratorio getLaboratorio() {
+		return laboratorio;
+	}
+
+	public Pecas[] getPecas() {
+		return pecas;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public void setLaboratorio(Laboratorio laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public void setPecas(Pecas[] pecas) {
+		this.pecas = pecas;
+	}
+
 }
--- ILP007/java/aula12_atividade04/src/Laboratorio.java
+++ ILP007/java/aula12_atividade04/src/Laboratorio.java
@@ -1,4 +1,23 @@
 
 public class Laboratorio {
 
+	private String sigla;
+	private String nome;
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
