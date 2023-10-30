b2f6e39e55346de17cc454c7a8aee7730b506565
Leonardo Setti - Black USB <devnull-vortex@protonmail.com>
2023-10-30 14:18:16 -0300

    :pencil: POO tarefa02 Aula 13 commit inicial Linux blackusb 6.5.5-arch1-1 - 2023103014181670692: eb70c6cc5653e412ded644cb6d61fcba33346faa48654b3cf4949157242d138d  -

---
+++ ILP007/java/aula13_tarefa02/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	
+	public void inputData() {
+		
+	}
+	
+	public void outputData() {
+		
+	}
+	
+	public static void main(String [] args) {
+		Principal p = new Principal();
+		p.inputData();
+		p.outputData();
+	}
+	
+}
