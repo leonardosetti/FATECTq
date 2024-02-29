8c08ce12c869af2d23240402f7a1197a6254ad9f
Leonardo Setti <srvsecsub@protonmail.com>
2024-02-29 14:53:34 -0300

    setting Oracle / SQL Scripts for IBD100

---
+++ 2024/sem_1/IBD100/scripts/inserts_AREA.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  Arquivo criado - Terça-feira-Outubro-04-2022   
+--------------------------------------------------------
+REM INSERTING into AREA
+SET DEFINE OFF;
+Insert into AREA (AREA_CODIGO,NOME_AREA,TIPO_AREA) values ('10','PRODUÇÃO','INDUSTRIAL');
+Insert into AREA (AREA_CODIGO,NOME_AREA,TIPO_AREA) values ('20','COMERCIAL','VENDAS');
+Insert into AREA (AREA_CODIGO,NOME_AREA,TIPO_AREA) values ('30','INVESTIMENTOS','FINANCEIRA');
+Insert into AREA (AREA_CODIGO,NOME_AREA,TIPO_AREA) values ('40','PROJETOS','INDUSTRIAL');
+Insert into AREA (AREA_CODIGO,NOME_AREA,TIPO_AREA) values ('50','NOVOS PRODUTOS','INDUSTRIAL');
