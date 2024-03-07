c54e86af96d4decdef0cb70a4b5e3c05e9797812
Leonardo Setti <srvsecsub@protonmail.com>
2024-03-07 14:07:57 -0300

    dbeaver study

--- 2024/sem_1/IBD100A/LAB_BD_2024-script-aula2.sql
+++ 2024/sem_1/IBD100A/LAB_BD_2024-script-aula2.sql
@@ -1,9 +1,3 @@
-/*
-Esquema (Schema) é uma coleção de objetos de banco de dados (Tabelas, Índices, Views, Procedures, etc), o qual prtence 
-a um usuário com o mesmo nome (Owner)
-
-Privilégio de objeto: permissão para manipulação de outros esquemas:
-GRANT SELECT, UPDATE, INSERT, DELET ON SCHEMA_NAME.OBJECT
-*/
-
-select * from area;
+SELECT NOME_AREA, NOME_USUARIO, DESC_SETOR FROM SETOR S 
+JOIN AREA A ON A.AREA_CODIGO IN S.AREA_CODIGO
+LEFT JOIN USUARIO U ON U.CODSETOR  IN S.CODSETOR;
