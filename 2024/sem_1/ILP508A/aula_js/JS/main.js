219ccefae5546c880fb8b712864ec0153fb6f4f4
Leonardo Setti <leonardo.setti@fatec.sp.gov.br>
2024-04-04 20:26:06 -0300

    manipulando DOM com JS

---
+++ 2024/sem_1/ILP508A/aula_js/JS/main.js
@@ -0,0 +1,11 @@
+function alerta() {
+    alert('Dios mio!');
+  }
+
+  function trocatexto(){
+    document.getElementById("_titulo").innerHTML="04/04/2024 - AULA JavaScript";
+  }
+
+  function setCss(){
+    document.getElementById("_titulo").classList.add("tituloStyle");
+  }
