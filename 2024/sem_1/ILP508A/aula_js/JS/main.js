0c77870d4c8553341cc303a2dac5903dd7c170d0
Leonardo Setti <leonardo.setti@fatec.sp.gov.br>
2024-04-04 21:41:42 -0300

    operando lógica com JS

--- 2024/sem_1/ILP508A/aula_js/JS/main.js
+++ 2024/sem_1/ILP508A/aula_js/JS/main.js
@@ -1,11 +1,28 @@
 function alerta() {
-    alert('Dios mio!');
-  }
+  alert("Dios mio!");
+}
 
-  function trocatexto(){
-    document.getElementById("_titulo").innerHTML="04/04/2024 - AULA JavaScript";
-  }
+function trocatexto() {
+  document.getElementById("_titulo").innerHTML = "04/04/2024 - AULA JavaScript";
+}
 
-  function setCss(){
-    document.getElementById("_titulo").classList.add("tituloStyle");
-  }
+function setCss() {
+  document.getElementById("_titulo").classList.add("tituloStyle");
+}
+
+function trocaCor() {
+  document.getElementById("_titulo").style.color = "Yellow";
+}
+
+function trocaTextoClasse() {
+  var tagsH3 = document.querySelectorAll(".subTitulo3");
+}
+
+function trocaTextoH2() {
+  document.querySelectorAll("h2").innerHTML = "Trocando o texto do h2";
+}
+
+for (var i = 0; i < tagsH3.length; i++) {
+  tagsH3[i].innerHTML = "Trocando o texto da classe";
+  tagsH3[i].style.color = "blue";
+}
